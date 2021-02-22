c15ab9686d14bd7e72ffcf889e52d78ccac995e7
JalenDurr9 <77880306+JalenDurr9@users.noreply.github.com>
2021-02-22 10:35:43 -0600

    Add files via upload

---
+++ Module 2 1,2,3.py
@@ -0,0 +1,16 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> print("Hi, What's Your Name?")
+Hi, What's Your Name?
+>>> print("Hello, User")
+Hello, User
+>>> print("Give Two Numbers to find the sum and product")
+Give Two Numbers to find the sum and product
+>>> Sum = int(input("Enter A+B")
+>>> Product = int(input("Enter A*B")
+		  
+SyntaxError: invalid syntax
+>>> Product = int(input("Enter A*B")
+	      
